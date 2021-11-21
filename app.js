0603f05195aa2998faf08415bbdc982853f3a848
stebellio <69798033+stebellio@users.noreply.github.com>
2021-11-21 15:06:50 +0100

    [new] Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen('80', () => {
+    console.log('Server listening on port 80');
+});
+
